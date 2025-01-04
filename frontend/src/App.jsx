e596d319c79e5e2f0578477173c5f17af1f8273c
prathamkchandra <prathamkappu565@gmail.com>
2025-01-04 14:40:48 +0530

    final commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
 import "./css/App.css";
-import Navbar from "./components/Navbar";
-import Favorite from "./pages/Favorite";
+import Favorites from "./pages/Favorite";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>  
-     <Navbar/>
-       <main className="main-content">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/favorites" element={<Favorite />} />
-    </Routes>
-  </main>
-  </div>
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
   );
 }
+
 export default App;
--- frontend/src/components/Moviecard.jsx
+++ frontend/src/components/Moviecard.jsx
@@ -1,25 +1,30 @@
-import "../css/Moviecard.css";
+import "../css/MovieCard.css"
+import { useMovieContext } from "../contexts/MovieContext"
 
-function Moviecard({ movie }) {
-  function onFavoriteClick() {
-    alert("clicked");
-  }
+function MovieCard({movie}) {
+    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext()
+    const favorite = isFavorite(movie.id)
 
-  return (
-    <div className="movie-card">
-      <div className="movie-poster">
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
-        <div className="movie-overlay">
-          <button className="favorite-btn" onClick={onFavoriteClick}>
-            ♥
-          </button>
+    function onFavoriteClick(e) {
+        e.preventDefault()
+        if (favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
+    }
+
+    return <div className="movie-card">
+        <div className="movie-poster">
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+            <div className="movie-overlay">
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
+                    ♥
+                </button>
+            </div>
         </div>
-      </div>
-      <div className="movie-info">
-        <h1>{movie.title}</h1>
-        <p>{movie.release_date}</p>
-      </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release_date?.split("-")[0]}</p>
+        </div>
     </div>
-  );
 }
-export default Moviecard;
+
+export default MovieCard;
